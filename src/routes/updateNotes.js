486d03b7b761efb83d18491efc9710d9f8239552
maheshhp <hpmahesh73@gmail.com>
2018-02-19 18:40:46 +0530

    Added handler for updateNotes

--- src/routes/updateNotes.js
+++ src/routes/updateNotes.js
@@ -1,11 +1,36 @@
+const Models = require('../../models');
+
+const updateNotesInDB = (noteObjectArray) => {
+  const dbInsertPromises = [];
+  noteObjectArray.forEach((noteObject) => {
+    dbInsertPromises.push(Models.Note.upsert({
+      noteTitle: noteObject.noteTitle,
+      noteText: noteObject.noteText,
+    }, {
+      where: {
+        noteId: noteObject.noteId,
+      },
+    }));
+  });
+  return Promise.all(dbInsertPromises);
+};
+
 module.exports = [
   {
     method: 'POST',
     path: '/notes/update',
     handler: (request, response) => {
-      response({
-        statusCode: 200,
-        message: 'O notes',
+      updateNotesInDB(request.payload).then((updateResponse) => {
+        console.log(updateResponse);
+        response({
+          statusCode: 201,
+          message: updateResponse,
+        });
+      }).catch((error) => {
+        response({
+          statusCode: 200,
+          message: error,
+        });
       });
     },
   },
