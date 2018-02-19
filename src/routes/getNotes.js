c66559213010c2ca3e7b7c683adcf87c4dd1492d
maheshhp <hpmahesh73@gmail.com>
2018-02-19 18:07:05 +0530

    Added handler for getNotes

--- src/routes/getNotes.js
+++ src/routes/getNotes.js
@@ -1,11 +1,32 @@
+const Models = require('../../models');
+
+const getNotesFromDB = () => Models.Note.findAll();
+
 module.exports = [
   {
     method: 'GET',
     path: '/notes/all',
     handler: (request, response) => {
-      response({
-        statusCode: 200,
-        message: 'O notes',
+      getNotesFromDB().then((noteObjects) => {
+        const formattedNoteObjects = [];
+        noteObjects.forEach((noteObject) => {
+          formattedNoteObjects.push({
+            noteId: noteObject.noteId,
+            noteText: noteObject.noteText,
+            noteTitle: noteObject.noteText,
+          });
+        });
+        console.log(formattedNoteObjects);
+        response({
+          statusCode: 200,
+          message: `${formattedNoteObjects.length} notes fetched`,
+          notes: formattedNoteObjects,
+        });
+      }).catch((err) => {
+        response({
+          statusCode: 200,
+          message: err,
+        });
       });
     },
   },
